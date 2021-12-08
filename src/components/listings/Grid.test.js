6887d2b79728d7348ff5de5ed72f6973c7f46e81
mrandolph83 <mrandolph83@gmail.com>
2021-12-08 13:45:34 -0500

    Add string matchers to Grid.js

--- src/components/listings/Grid.test.js
+++ src/components/listings/Grid.test.js
@@ -1,5 +1,6 @@
 import data from '../../data/courses.json';
 
+// Number tests
 const numItems = data.length;
 
 test('Number of items = 12', () => {
@@ -9,4 +10,18 @@ test('Number of items = 12', () => {
 test('Number of items to be greater than or equal to 12', () => {
     expect(numItems).toBeGreaterThanOrEqual(12)
 });
+
+// String tests
+
+const stringTest = data[0].title; 
+
+test('There is a JS in this title', () => {
+    expect(stringTest).toMatch(/JS/);
+});
+// different matchers, full documentation available
+test('The title contains React', () => {
+    expect(stringTest).toContain('React');
+});
+
+
 
